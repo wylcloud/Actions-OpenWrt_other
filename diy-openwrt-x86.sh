7ea123ec2f6c689efc9f8f644637349fdb37119e
wylcloud <82373611+wylcloud@users.noreply.github.com>
2023-11-10 23:52:25 +0800

    Update diy-openwrt-x86.sh

--- diy-openwrt-x86.sh
+++ diy-openwrt-x86.sh
@@ -17,7 +17,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci
 #sed -i "s/OpenWrt/Deng Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 10.10.10.1）
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
 
